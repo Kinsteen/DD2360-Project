5baf22a9d44c61683f1cc063f5caa3e7fd68451c
Kinsteen <pitiqui69@gmail.com>
2022-11-30 21:46:18 +0100

    Setup CUDA compilation

--- src/InOneWeekend/main.cu
+++ src/InOneWeekend/main.cu
@@ -88,7 +88,7 @@ hittable_list random_scene() {
 }
 
 
-int main() {
+__host__ int main() {
 
     // Image
 
@@ -117,7 +117,7 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height-1; j >= 0; --j) {
-        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        //std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
             color pixel_color(0,0,0);
             for (int s = 0; s < samples_per_pixel; ++s) {
